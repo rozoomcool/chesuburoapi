b8804ebd1f6360654d517e8925b58798a5b87e64
rozoomcool <rozoom.um@gmail.com>
2024-10-19 02:36:53 +0300

    setting media data

--- src/main/kotlin/com/rozoomcool/chguburoapi/entity/MediaData.kt
+++ src/main/kotlin/com/rozoomcool/chguburoapi/entity/MediaData.kt
@@ -6,4 +6,10 @@ import jakarta.persistence.*
 @Table(name = "media_data")
 class MediaData(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long? = null,
+
+    @Column(name = "filename", nullable = false, unique = true)
+    var filename: String,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    var owner: User? = null
 )
